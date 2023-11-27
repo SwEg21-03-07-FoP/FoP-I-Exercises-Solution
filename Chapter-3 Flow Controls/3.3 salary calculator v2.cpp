88b7f0b30b36fbcb1368ef583613570662311217
Chere Lemma <chere.lemma@aastu.edu.et>
2023-11-27 23:50:36 +0300

    Update 3.3 salary calculator v2.cpp

--- Chapter-3 Flow Controls/3.3 salary calculator v2.cpp
+++ Chapter-3 Flow Controls/3.3 salary calculator v2.cpp
@@ -1,1 +1,32 @@
+// A program that used to calculate the salary of an employee.
 
+#include <iostream>
+#include <iomanip>  // For setprecision
+using namespace std;
+
+int main() {
+    // Declare the required variables
+    int workingHours;
+    double bonusRate, baseSalary,  bonusPayment = 0.0;
+
+    // Input the details
+    cout << "Enter base salary: ";
+    cin >> baseSalary;
+  
+    cout << "Enter weekly working hours: ";
+    cin >> weeklyHours;
+
+    // Assume the normal working hours are 40 or less.
+    if (weeklyHours > 40) {
+          cout << "Enter bonus rate per hour: ";
+          cin >> bonusRate;
+          // Calculate gross salary and bonus payment
+          bonusPayment = weeklyHours * bonusRate;
+    }
+  
+    // Display results
+    cout << "Gross Salary: $" << fixed << setprecision(2) << baseSalary + bonusPayment  << endl;
+    cout << "Bonus Payment: $" << bonusPayment << endl;
+
+    return 0;
+}
