948fb09f73ff35e948ca2c7ee8e8b0a8f70adf2d
Chere Lemma <chere.lemma@aastu.edu.et>
2023-11-27 22:11:26 +0300

    Update 4 exponent caclutor.cpp

--- Chapter-3 Basic Programming Constructors/4 exponent caclutor.cpp
+++ Chapter-3 Basic Programming Constructors/4 exponent caclutor.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <cmath>  // For the pow function
+using namespace std;
 
+int main() {
+    // Declare variables
+    double x, y, result;
+
+    // Input values from the user
+    cout << "Enter the value of x: ";
+    cin >> x;
+
+    cout << "Enter the value of y: ";
+    cin >> y;
+
+    // Calculate x^y using the pow function from the cmath library
+    result = pow(x, y);
+
+    // Display the result
+    cout << "Result of " << x << "^" << y << " is: " << result << endl;
+
+    return 0;
+}
