6fc52f226af73448dc72ab5f98f3a816477bf92e
Chere Lemma <chere.lemma@aastu.edu.et>
2023-11-28 07:14:24 +0300

    Update 3.5 number checker v1(b).cpp

--- Chapter-3 Flow Controls/3.5 number checker v1(b).cpp
+++ Chapter-3 Flow Controls/3.5 number checker v1(b).cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int number;
+
+    // Prompt user to enter a number
+    cout << "Enter a number: ";
+    cin >> number;
+
+    // Check if the number is even or odd and positive or negative
+    if (number > 0) {
+        if (number % 2 == 0) {
+            cout<<"The "<<number<<" is even positive number." <<endl;
+        } else {
+            cout<<"The "<<number<<" is odd positive number." <<endl;
+        }
+    } else if (number < 0) {
+        if (number % 2 == 0) {
+            cout<<"The "<<number<<" is even negative number." <<endl;
+        } else {
+           cout<<"The "<<number<<" is odd negative number." <<endl;
+        }
+    } else {
+         cout << "The number is zero." <<endl;
+    }
+
+    return 0;
+}
 
