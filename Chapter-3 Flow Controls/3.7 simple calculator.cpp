b28fd4d5d77234c840b99d0c5af0e8997cc3ab05
Chere Lemma <chere.lemma@aastu.edu.et>
2023-12-04 11:51:21 +0300

    Update 3.7 simple calculator.cpp

--- Chapter-3 Flow Controls/3.7 simple calculator.cpp
+++ Chapter-3 Flow Controls/3.7 simple calculator.cpp
@@ -1,1 +1,5 @@
-# Coming Soon
+/* A simple calculator program in C++ that uses a switch statement to perform basic arithmetic operations.
+   The program will continue to take user input for operations until the user enters 0 to exit.
+*/
+
+The solution is Coming Soon
