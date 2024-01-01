81b68bf3cde21bc6b5240d660c35d4de41dcf06a
cherelemma <chere.lemma@aastu.edu.et>
2024-01-01 15:56:49 +0300

    1D array initialization

--- Chapter-4 Array and Strings/4.1 1D array initalization.cpp
+++ Chapter-4 Array and Strings/4.1 1D array initalization.cpp
@@ -5,5 +5,18 @@ using namespace std;
 
 int main() {
 
+  int day [5] = { 16, 2, 77, 40, 27 };
+  cou<<"The array elements are: "
+  for (int i = 0; i < 5; i++) {
+    cout<<day[i]<<", "
+  }
+  cout<<endl;
+  // omiting the array size
+  float my_weekend [] = {60, 73.5, 57.25, 80, 41.8, 67, 89, 54}
+    cou<<"My Weekend mod in % are: "
+  for (int i = 0; i < sizeof(my_weekend)/4; i++) {
+    cout<<my_weekend[i]<<", "
+  }
+  cout<<endl;
   return 0;
 }
